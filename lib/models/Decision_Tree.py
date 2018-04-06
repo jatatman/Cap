a9a523e3cd5943bd7540ffeb6cababe6b179c033
Justin Tatman <jatatman12@gmail.com>
2018-04-06 13:54:12 -0600

    notation added

--- lib/models/Decision_Tree.py
+++ lib/models/Decision_Tree.py
@@ -1,31 +1,32 @@
 import numpy as np
 
-def Class DecisionTree():
+class DecisionTree():
 
-	def __init__(self):
-		pass
+    def entropy(self, prob):
+        '''
+        Determines the entropy of a split.
+        '''
+        # replacing - values with 1 to avoid issues with np.log2 returning - inf
+        # both 0 and 1 represent pure splits so the result will not change
+        zero_mask = prob == 0
+        prob[zero_mask] = 1
 
-	def entropy(self, prob):
-    	'''
-
-    	'''
-    	# replacing - values with 1 to avoid issues with np.log2 returning - inf
-    	# both 0 and 1 represent pure splits so this change will not effect the result
-
-    	zero_mask = prob == 0
-    	prob[zero_mask] = 1
-
-    	log_2_prob = np.log2(prob)
-    	return -prob * log_2_prob
+        log_2_prob = np.log2(prob)
+        return -prob * log_2_prob
 
     def entropy_split(self, data, target, feature, debug=False):
+		'''
+		Determines which feature will result in the lowest entropy split.
+		'''
 
-        # splitting data along a feature
+        # splitting the data along a feature
         unique_class, class_counts = np.unique(data[:,feature], return_counts=True)
 
-        weights = class_counts/ target.shape[0]
+        weights = class_counts / target.shape[0]
 
+		### REMOVE DEBUGGING STATEMENTS FROM FINAL VERSION
         if debug: print('Weights\n{}\n'.format(weights))
+
         # getting the split proportions
         splits = [target[data[:,feature] == uc] for uc in unique_class]
 
@@ -37,56 +38,105 @@ def Class DecisionTree():
 
         if debug: print('Probability\n{}\n'.format(prob))
 
-        entropies = entropy(prob)
+        entropies = self.entropy(prob)
 
         if debug: print('entropies\n{}\n'.format(entropies))
 
-            # transposing entropies shape so it can be multiplied with weights
+        # transposing entropies shape so it can be multiplied with weights
         w_ent = entropies.T * weights
 
         return w_ent.sum()
 
-	def find_best_split(data, target):
+    def find_best_split(self, data, target, split_candidates):
+		'''
+		finds the best split for a data set
+		'''
 
-    	split_entropies = []
+        split_entropies = [self.entropy_split(data, target, split_candidate) for split_candidate in split_candidates]
+        splt_ent = np.array(split_entropies)
+        sort_ent_indx = splt_ent.argsort()
+        best_ent_indx = sort_ent_indx[0]
 
-    	for i in range(data.shape[1]):
-        	ent = entropy_split(data, target, i)
-        	split_entropies.append(ent)
+        return split_candidates[best_ent_indx]
 
-    	splt_ent = np.array(split_entropies)
-    return splt_ent.argsort()
+    def tree_builder(self, data, target, split_candidates=None):
+		'''
+		builds a decision tree
+		'''
 
-### from Data Science From Scratch
+        if split_candidates is None:
+            split_candidates = np.array([i for i in range(data.shape[1])])
 
-def build_tree_id3(inputs, split_candidates=None):
+        total_count = target.shape[0]
+        true_count = target.sum()
+        false_count = total_count - true_count
 
-	# if this is oru first pass,
-	# all keys of the first input are split split_candidates
-	if split canddiates is None:
-		split_candidates = inputs[0][0].keys()
-	# count Trues and Falses in the inputs
-	num_inputs = len(inputs)
-	num_trues = len([label for item, label in inputs if label])
-	num_falses = num_inputs - num_trues
+        if true_count == 0:
+            return False
+        elif false_count == 0:
+            return True
 
+        if split_candidates.shape[0] == 0:
+            return true_count > false_count
 
-	if num_trues == 0: return False # no Trues? return a "False" leaf
-	if num_falses == 0: return True # no Falses? return a "True" leaf
+        best_split = self.find_best_split(data, target, split_candidates)
+        split_candidates = np.array([split_candidate for split_candidate in split_candidates if split_candidate != best_split])
 
-	if not split_candidates: # if no split candidates left
-		return num_trues >= num_falses # return majority leaf
+        nodes = np.unique(data[:, best_split])
+        subtree_dict = dict()
 
-	# otherwise split on the best attributes
-	best_attribute = min(split_candidates,
-	key=partial(partition_entropy_by, inputs))
+        for node in nodes:
+            sub_mask = data[:, best_split] == node
+            data_sub = data[sub_mask]
+            tar_sub = target[sub_mask]
 
-	parititons = partition_by(inputs, best_attribute)
-	new_canddidates = [a for a in split_candidates
-	if a != best_attribute]
+            subtree_dict[node] = self.tree_builder(data_sub, tar_sub, split_candidates)
 
-	# recursively build the subtrees
-	subtrees = { attribute_value : build_tree_id3(subset, new_candidates)
-	for attribute_value, subset in partition.iteritems()}
+        subtree_dict[None] = true_count > false_count
 
-	subtreees]None = num_trues > num_falses # default case
+        return(self.features[best_split], subtree_dict)
+
+    def fit(self, data, target):
+		'''
+		fits a decision tree to a data set.
+		'''
+
+        self.features = np.array(data.columns)
+
+        data = np.array(data)
+        target = np.array(target)
+
+        self.tree = self.tree_builder(data, target)
+
+    def predict(self, tree, element):
+		'''
+		predicts values for a single element in a data set
+		'''
+
+        if tree in [True, False]:
+            return tree
+
+        node, branches = tree
+        branch = element[node]
+
+        if branch not in branches:
+            branch = None
+
+        sub_tree = branches[branch]
+
+        return self.predict(sub_tree, element)
+
+    def score(self, data, target):
+        '''
+		determines the accuracy of the models predictions
+		'''
+
+		# gets predictions for every observations in the data
+        self.tree_predictions = [self.predict(self.tree, data.iloc[i,:]) for i in range(data.shape[0])]
+
+        true_count = 0
+
+        for pred, actual in zip(self.tree_predictions, target):
+            if pred == actual:
+                true_count += 1
+        return true_count / len(self.tree_predictions)
